e85fa7f66cb7cc556d522b856100e710cd0fa903
Libra-nti <librantiantonio04@gmail.com>
2025-03-23 13:09:41 +0100

    aggiunta possibilità di scorrere le pagine sia normalmente che filtrate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var J = 0
 var url = "https://trekking-qwju.onrender.com"
 
 var src = []
-
+var pages
 
 
 async function loading(){
@@ -20,12 +20,15 @@ async function loading(){
         console.log(data)
         trekkingData = data;
         filteredTrekking = data;
-        renderTrekkingList(filteredTrekking)
-        console.log(filteredTrekking)
-        newest()
+       renderTrekkingList(filteredTrekking, 1)
+        
+
+        //console.log(filteredTrekking)
+        //newest()
 
     })
     document.getElementById("loader").style.display = "none"
+    document.getElementById("content").style.display = "block"
   }
 
 
@@ -51,19 +54,27 @@ function filterTrekking() {
     });
     console.log(filteredTrekking)
 
-    renderTrekkingList(filteredTrekking);
+    renderTrekkingList(filteredTrekking, 1);
 }
 
 // Funzione per visualizzare i trekking
-function renderTrekkingList(trekkingList) {
-
-    console.log("trekkingList")
+function renderTrekkingList(trekkingList, pageSelected) {
+    var c = 0
+    console.log(trekkingList)
     const listContainer = document.getElementById('trekking-list');
     listContainer.innerHTML = ''; // Pulisce la lista esistente
     var H = 0;
     var row
-    trekkingList.forEach(trekking => {
-        console.log(trekking.src)
+    var maxShowed
+    if(trekkingList.length<12*pageSelected){
+        maxShowed = trekkingList.length
+    }
+    else{
+        maxShowed = 12*pageSelected
+    }
+    for(var pagesI=12*(pageSelected-1); pagesI<maxShowed;pagesI++){
+        
+        //console.log(pagesI)
         if (H % 6 == 0) {
             row = document.createElement("div")
             row.className = "row"
@@ -73,25 +84,28 @@ function renderTrekkingList(trekkingList) {
         card.className = "col-sm-4 col-lg-2 col-6 mb-3"
         card.innerHTML = `
     <div class="card h-100" style="width: auto">
-      <img src='${trekking.url}' class="card-img-top">
+      <img src='${trekkingList[pagesI].url}' class="card-img-top">
        <div class="card-body bottom-0">
-       <h5 class="card-title">${trekking.name}</h5>
-       <p class="card-text">${trekking.date}</p>
+       <h5 class="card-title">${trekkingList[pagesI].name}</h5>
+       <p class="card-text">${trekkingList[pagesI].date}</p>
       </div>
       </div>
     `;
         card.onclick = () => {
-            window.location.href = `trekking-details.html?id=${trekking._id}`;
+            window.location.href = `trekking-details.html?id=${trekkingList[pagesI]._id}`;
         };
         row.appendChild(card)
         H = H + 1
 
-    });
+    }
+    pages = Math.ceil(trekkingList.length/12)
+    console.log(pages)
+
 }
 
 // Funzione per caricare i dettagli di un trekking
 
-
+/* 
 function newest(){
     var  newest = "1999-01-01"
     var ind
@@ -108,4 +122,47 @@ function newest(){
     newLogo.style.width="100%"
     t.appendChild(newLogo)
     
+}
+
+ */
+
+function next(){
+    var items
+    if(document.getElementById("distance").value != '' || document.getElementById("elevation").value != '' ||document.getElementById("season").value != '' ||document.getElementById("difficulty").value != '' ||document.getElementById("date").value != ''){
+        items = filteredTrekking
+       
+    }
+    else{
+        items = trekkingData
+    }
+    var pagesEl = document.getElementById("pages")
+    var actualPage = pagesEl.innerText
+    console.log(pages)
+    if(pages>1 && pagesEl.innerText!=pages){
+        console.log("qui")
+        actualPage++
+        pagesEl.innerText = actualPage
+        console.log(pagesEl.innerText)
+        renderTrekkingList(items, pagesEl.innerText)
+    }
+    
+}
+
+
+function prev(){
+    var items
+    if(document.getElementById("distance").value != '' || document.getElementById("elevation").value != '' ||document.getElementById("season").value != '' ||document.getElementById("difficulty").value != '' ||document.getElementById("date").value != ''){
+        items = filteredTrekking
+    }
+    else{
+        items = trekkingData
+    }
+    var pagesEl = document.getElementById("pages")
+    var actualPage = pagesEl.innerText
+    if(pagesEl.innerText!=1){
+        actualPage--
+        pagesEl.innerText = actualPage
+        renderTrekkingList(items, pagesEl.innerText)
+    }
+
 }
