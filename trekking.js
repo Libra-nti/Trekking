f16a83e6e345f4c173d2702d9b4030124f6b9e9f
Libra-nti <librantiantonio04@gmail.com>
2025-05-31 11:40:33 +0200

    bivacchi v3

--- trekking.js
+++ trekking.js
@@ -99,15 +99,15 @@ const east = bounds.getEast();
 
 const queryOverpass = `[out:json][timeout:25];
 (
-  node["tourism"~"alpine_hut|wilderness_hut"](south,west,north,east);
-  way["tourism"~"alpine_hut|wilderness_hut"](south,west,north,east);
-  relation["tourism"~"alpine_hut|wilderness_hut"](south,west,north,east);
+  node["tourism"~"alpine_hut|wilderness_hut"](`+south+`,`+west+`,`+north+`,`+east+`);
+  way["tourism"~"alpine_hut|wilderness_hut"](`+south+`,`+west+`,`+north+`,`+east+`);
+  relation["tourism"~"alpine_hut|wilderness_hut"](`+south+`,`+west+`,`+north+`,`+east+`);
 
-  node["amenity"="shelter"](south,west,north,east);
-  way["amenity"="shelter"](south,west,north,east);
-  relation["amenity"="shelter"](south,west,north,east);
+  node["amenity"="shelter"](`+south+`,`+west+`,`+north+`,`+east+`);
+  way["amenity"="shelter"](`+south+`,`+west+`,`+north+`,`+east+`);
+  relation["amenity"="shelter"](`+south+`,`+west+`,`+north+`,`+east+`);
 
-  node["name"~"bivacco|rifugio",i](south,west,north,east);
+  node["name"~"bivacco|rifugio",i](`+south+`,`+west+`,`+north+`,`+east+`);
 );
 out body;
 >;
@@ -115,7 +115,7 @@ out skel qt;`
 
 fetch('https://overpass-api.de/api/interpreter', {
   method: 'POST',
-  body: query,
+  body: queryOverpass,
   headers: {
     'Content-Type': 'text/plain'
   }
