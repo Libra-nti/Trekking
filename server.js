12d5cc13916e1a4463df32291ba952782a6cb5db
Libra-nti <librantiantonio04@gmail.com>
2025-08-02 16:04:13 +0200

    vaffanculo ho sbagliato il sito

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.get('/sitemap.xml', async (req, res) => {
     console.log(trekkingList)
     const urls = trekkingList.map(trekking => `
         <url>
-            <loc>https://viaggiditony.com/trekking/${trekking.name.toLowerCase()}-${trekking._id}</loc>
+            <loc>https://viaggiditony.onrender.com/trekking/${trekking.name.toLowerCase()}-${trekking._id}</loc>
         </url>
     `).join('');
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://viaggiditony.com/</loc></url>
+  <url><loc>https://viaggiditony.onrender.com/</loc></url>
   ${urls}
 </urlset>`;
 
